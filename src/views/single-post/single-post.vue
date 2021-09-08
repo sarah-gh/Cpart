239bea375c985b33666e90dd2bc6eddf1f4d25c3
sara.ghotbzadeh <sara.ghotbzadeh@partsoftware.com>
2021-09-08 10:56:49 +0430

    add component for new comment

--- src/views/single-post/single-post.vue
+++ src/views/single-post/single-post.vue
@@ -10,6 +10,7 @@
                 <div class="boldLine"></div>
                 <div class="line"></div>
             </div>
+            
             <div class="other-articles">
                 <article class="article" v-for="(item, index) in otherPosts" :key="index">
                     <post-this-author :post="item"></post-this-author>
@@ -23,32 +24,13 @@
                 <div class="boldLine"></div>
                 <div class="line"></div>
             </div>
-            <div class='comments' v-for="(comment, i) in comments" :key="i">
-                <post-comment :comment="comment"></post-comment>
+
+            <div class='comments' v-for="(com, i) in comments" :key="i">
+                <post-comment :comment="com"></post-comment>
                 <div class="commentLine"></div>
             </div>
 
-            <form action="" class="form-comment">
-                <div class="fname form-item">
-                    <label for="fname" class="label">نام*</label>
-                    <input class="input" type="text" id="fname" name="fname">
-                </div>
-                <div class="lname form-item">
-                    <label for="lname" class="label">نام خانوادگی*</label>
-                    <input class="input" type="text" id="lname" name="lname">
-                </div>
-                <div class="email form-item">
-                    <label for="email" class="label">ایمیل*</label>
-                    <input class="input" type="email" id="email" name='email'>
-                </div>
-                <div class='user-comment form-item'>
-                    <label for="user-comment" class="label">نظر دادن*</label>
-                    <textarea class="input" id="user-comment" name='user-comment'></textarea>
-                </div>
-                <div class="submit">
-                    <p>ثبت نظر</p>
-                </div>
-            </form>
+            <post-new-comment></post-new-comment>            
             
         </div>
     </main>
