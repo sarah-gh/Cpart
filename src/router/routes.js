ee115fe09a2fa51cac9d594aa52f0d8590175ec7
mohammad vatani <mohammad.vn78@gmail.com>
2021-09-17 17:02:19 +0430

    fix errors on register pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -31,6 +31,10 @@ export const routes = [
     component: () => import(/* webpackChunkName: "single-post" */ '../views/single-post/single-post.vue')
   },
   {
+    path: "/register",
+    component: () => import(/* webpackChunkName: "not-found" */ '../views/register/1/register.vue')
+  },
+  {
     path: "/:catchAll(.*)",
     component: () => import(/* webpackChunkName: "not-found" */ '../views/not-found/not-found.vue')
   },
--- src/views/register/2/register.vue
+++ src/views/register/2/register.vue
@@ -6,13 +6,13 @@
       <div class="green change-number">اصلاح شماره تلفن</div>
     </div>
     <div class="register-code">
-      <div>لطفا کد ۵ رقمی ارسال شده را وارد نمایید</div>
+      <div class="register-guide">لطفا کد ۵ رقمی ارسال شده را وارد نمایید</div>
       <div class="digit-box">
-        <!-- v for -->
+          <num-input class="digit" v-for="index in 5" :key="index"></num-input>
       </div>
     </div>
     <button type="submit" class="button">تایید</button>
-    <div class="remained-time">زمان باقیمانده {{ leftTime }} دقیقه</div>
+    <div class="remained-time">زمان باقیمانده {{ leftTime }}</div>
     <div class="recieve-code">
       کد دریافت نکرده اید؟ <span class="green send-again">ارسال مجدد</span>
     </div>
@@ -20,16 +20,29 @@
 </template>
 
 <script>
+import numInput from "../../../resources/components/register/codeButton/button.vue";
 export default {
+  components: {
+    numInput,
+  },
+  created: function timer() {
+    const timerInterval = setInterval(() => {
+      this.leftTime -= 1;
+      if (this.leftTime === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  },
+
   data() {
     return {
-      leftTime: "۲",
+      leftTime: 120,
       phoneNumber: "۰۹۱۲۱۲۳۴۳۲۰",
+      numbers : [1,2,3,4]
     };
   },
 };
 </script>
 
 <style scoped lang="scss" src="./register.scss">
-
 </style>
