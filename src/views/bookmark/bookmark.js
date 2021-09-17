4b4c5d621d62bc2d45e9e81bb46e1b709598c1b8
mohammad vatani <mohammad.vn78@gmail.com>
2021-09-17 22:51:56 +0430

    connect bookmark page with server

--- src/views/bookmark/bookmark.js
+++ src/views/bookmark/bookmark.js
@@ -2,53 +2,27 @@ import postSummary from '../../resources/components/post/post-summary-bookmark/p
 
 export default {
     name: 'posts-summary',
-    data(){
+    data() {
         return {
-            items: [1,2,3],
-            posts: [
-                {
-                    imgPost : 'Capture1.png',
-                    imgAvatar : '6893547.png"',
-                    authorName : 'محمد محمدی',
-                    title : `کنترل کننده زیردریایی طراحی شده توسط دانشجویان دانشگاه صنعتی شریف
-                    برای ارتش جمهوری اسلامی ایران در بین ۱۰ زیردریایی برتر جهان قرار گرفت.
-                    `,
-                    text: ` کنترل کننده زیردریایی طراحی شده توسط دانشجویان دانشگاه صنعتی شریف
-                    برای ارتش جمهوری اسلامی ایران در بین ۱۰ زیردریایی برتر جهان قرار گرفت.
-                    `,
-                    date: '۲۸ تیر ۱۴۰۰',
-                    time: '۷ دقیقه مطالعه',
-                    tags: ['تکنولوژی']
-                },
-                {
-                    imgPost : 'Capture2.png',
-                    imgAvatar : '6893547.png"',
-                    authorName : 'محمد محمدی',
-                    title : `کنترل کننده زیردریایی طراحی شده توسط دانشجویان دانشگاه صنعتی شریف
-                    برای ارتش جمهوری اسلامی ایران در بین ۱۰ زیردریایی برتر جهان قرار گرفت.
-                    `,
-                    text: ` کنترل کننده زیردریایی طراحی شده توسط دانشجویان دانشگاه صنعتی شریف
-                    برای ارتش جمهوری اسلامی ایران در بین ۱۰ زیردریایی برتر جهان قرار گرفت.
-                    `,
-                    date: '۲۸ تیر ۱۴۰۰',
-                    time: '۷ دقیقه مطالعه',
-                    tags: ['تکنولوژی']
-                },
-                {
-                    imgPost : 'Capture3.png',
-                    imgAvatar : '6893547.png"',
-                    authorName : 'محمد محمدی',
-                    title : `کنترل کننده زیردریایی طراحی شده توسط دانشجویان دانشگاه صنعتی شریف
-                    برای ارتش جمهوری اسلامی ایران در بین ۱۰ زیردریایی برتر جهان قرار گرفت.
-                    `,
-                    text: ` کنترل کننده زیردریایی طراحی شده توسط دانشجویان دانشگاه صنعتی شریف
-                    برای ارتش جمهوری اسلامی ایران در بین ۱۰ زیردریایی برتر جهان قرار گرفت.
-                    `,
-                    date: '۲۸ تیر ۱۴۰۰',
-                    time: '۷ دقیقه مطالعه',
-                    tags: ['تکنولوژی']
-                }
-            ]
+            posts: []
+        }
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        async getPosts() {
+            try {
+                const response = await this.axios.get(
+                    `http://localhost:8000/api/users/savedMessages/${this.$route.params.id}`
+                ).then((res) => {
+                    return res.data;
+                });
+                console.log(response);
+                this.posts = response;
+            } catch (error) {
+                console.log(error);
+            }
         }
     },
     components: {
