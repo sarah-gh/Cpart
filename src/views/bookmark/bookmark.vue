47ecb56612640232a67aab2e133a430c7d9a07d5
sara.ghotbzadeh <sara.ghotbzadeh@partsoftware.com>
2021-09-25 11:30:57 +0330

    add loading in bookmark

--- src/views/bookmark/bookmark.vue
+++ src/views/bookmark/bookmark.vue
@@ -1,11 +1,21 @@
 <template>
     <main>
-        <div class="saved-message container">
+        <section class="load" v-if="!load">
+            <div class="load2">
+                <hr/><hr/><hr/><hr/>
+            </div>
+        </section>
+        <section class="post-container container load connection" v-if="!connection">
+            <p>
+                ارتباط با سرور قطع شده است. لطفا اتصال اینترنت خود را بررسی کنید
+            </p>
+        </section>
+        <div class="saved-message container"  v-if="load && connection">
             <img src="@/assets/img/svg-post/saved-messages.svg" alt="">
             <h2>مقالات ذخیره شده</h2>
         </div>
-        <section class="post-container container">
-            <article class="card" v-for="(item,index) in posts" :key="index" >
+        <section class="post-container container" v-if="load && connection">
+            <article class="card" v-for="(item,index) in posts" :key="index">
                 <post-summary-bookmark :post="item"></post-summary-bookmark>
             </article>
         </section>
